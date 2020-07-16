2f4e3a8308bcee6c9f0ecf38619c4bceb90cd102
ssurananitish <surananitish@gmail.com>
2020-07-16 17:09:22 +0530

    Made few Changes in the code

--- utils.py
+++ utils.py
@@ -1,34 +1,21 @@
 from numpy import expand_dims, ones, zeros, vstack
 from numpy.random import randint, rand, randn
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Dropout, Flatten, LeakyReLU, Conv2DTranspose, Reshape
-from keras.optimizers import Adam
-from keras.datasets.mnist import load_data
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Dropout, Flatten, LeakyReLU, Conv2DTranspose, Reshape
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.datasets.mnist import load_data
 import matplotlib.pyplot as plt
 
 def vis_images(dataset):
+    # Function for visualizing the dataset
     for i in range(15):
         plt.subplot(5,3,i+1)
         plt.axis('off')
         plt.imshow(dataset[i],cmap='gray_r')
     plt.show()
 
-def discriminator_arch(in_shape=(28,28,1)):
-    model = Sequential()
-    model.add(Conv2D(64, (3,3), strides=(2,2), padding="same",input_shape = in_shape))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(Dropout(0.3))
-    model.add(Conv2D(64, (3,3), strides=(2,2), padding="same"))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(Dropout(0.3))
-    model.add(Flatten())
-    model.add(Dense(1, activation="sigmoid"))
-    
-    optimizer = Adam(learning_rate=0.0001, beta_1=0.5)
-    model.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=["accuracy"])
-    return model
-
 def load_real_samples():
+    # Loading the real examples from the MNIST dataset
     (trainX, _), (_,_) = load_data()
     trainX = expand_dims(trainX, axis=-1)
     trainX = trainX.astype('float32')
@@ -36,27 +23,27 @@ def load_real_samples():
     return trainX
 
 def generated_real_samples(dataset, n_samples):
+    # Fucntion for randomly selecting real data samples
     i = randint(0,dataset.shape[0], n_samples)
     sample = dataset[i]
     label = ones((n_samples,1))
     return sample, label
 
 def generated_fake_samples(g_model, latent_dim, n_samples):
+    # Function for generating fake data samples
     fake_sample = generated_latent_points(latent_dim, n_samples)
     prediction = g_model.predict(fake_sample)
     label = zeros((n_samples, 1))
     return prediction, label
 
-def train_discriminator(model, dataset, n_iter=100,n_batch=256):
-    half_batch = int(n_batch/2)
-    for i in range(n_iter):
-        X_real, y_real = generated_real_samples(dataset, half_batch)
-        _, real_accuracy = model.train_on_batch(X_real, y_real)
-        X_fake, y_fake = generated_fake_samples(half_batch)
-        _, fake_accuracy = model.train_on_batch(X_fake, y_fake)
-        print('{}: real={}% \t fake={}%'.format(i+1, real_accuracy*100, fake_accuracy*100))
+def generated_latent_points(latent_dim, n_samples):
+    # Function for generating latent points within to latent_dim
+    point = randn(latent_dim * n_samples)
+    point = point.reshape(n_samples, latent_dim)
+    return point
 
 def generator_arch(latent_dim):
+    # Defining the Generator Architecture used
     model = Sequential()
     n_nodes = 128*7*7
     model.add(Dense(n_nodes, input_dim = latent_dim))
@@ -69,12 +56,23 @@ def generator_arch(latent_dim):
     model.add(Conv2D(1, (7,7),activation="sigmoid", padding="same"))
     return model
 
-def generated_latent_points(latent_dim, n_samples):
-    point = randn(latent_dim * n_samples)
-    point = point.reshape(n_samples, latent_dim)
-    return point
+def discriminator_arch(in_shape=(28,28,1)):
+    # Defining the Discriminator Architecture Used
+    model = Sequential()
+    model.add(Conv2D(64, (3,3), strides=(2,2), padding="same",input_shape = in_shape))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.3))
+    model.add(Conv2D(64, (3,3), strides=(2,2), padding="same"))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.3))
+    model.add(Flatten())
+    model.add(Dense(1, activation="sigmoid"))
+    optimizer = Adam(learning_rate=0.0001, beta_1=0.5)
+    model.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=["accuracy"])
+    return model
 
 def GAN_arch(g_model, d_model):
+    # Defining the GAN Architecture used
     d_model.trainable = False
     model = Sequential()
     model.add(g_model)
@@ -83,13 +81,8 @@ def GAN_arch(g_model, d_model):
     model.compile(optimizer=optimizer, loss="binary_crossentropy")
     return model
 
-def train_GAN(gan_model, latent_dim, n_batch=256, n_epochs=100):
-    for i in range(n_epochs):
-        x_gan = generated_latent_points(latent_dim,n_batch)
-        y_gan = ones((n_batch,1))
-        gan_model.train_on_batch(x_gan,y_gan)
-
 def train(g_model, d_model, gan_model, dataset, latent_dim, n_epochs=100, n_batch=256):
+    # Function responsible for the training of the GAN
     bat_per_epoch = int(dataset.shape[0]/ n_batch)
     half_batch = int(n_batch/2)
     for i in range(n_epochs):
@@ -107,6 +100,7 @@ def train(g_model, d_model, gan_model, dataset, latent_dim, n_epochs=100, n_batc
             summarize_performance(i, g_model, d_model, dataset, latent_dim)
 
 def summarize_performance(epoch, g_model, d_model, dataset, latent_dim, n_samples = 100):
+    # Function used for viewing the accuracy metrics and saving model at different epochs
     X_real, y_real = generated_real_samples(dataset, n_samples)
     _, real_accuracy = d_model.evaluate(X_real, y_real, verbose = 0)
     X_fake, y_fake = generated_fake_samples(g_model, latent_dim, n_samples)
@@ -117,6 +111,7 @@ def summarize_performance(epoch, g_model, d_model, dataset, latent_dim, n_sample
     g_model.save(filename)
 
 def save_plot(examples, epoch, n=10):
+    # Visualizing the output from the generator
     for i in range(n*n):
         plt.subplot(n,n,i+1)
         plt.axis('off')
